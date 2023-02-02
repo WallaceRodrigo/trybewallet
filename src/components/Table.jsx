d9959612090bf3e16ee88a81e90685bce8acf0f4
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2023-02-02 08:55:35 -0300

    6 table headers

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,8 +2,28 @@ import React, { Component } from 'react';
 
 class Table extends Component {
   render() {
+    const headers = [
+      'Descrição',
+      'Tag',
+      'Método de pagamento',
+      'Valor',
+      'Moeda',
+      'Câmbio utilizado',
+      'Valor convertido',
+      'Moeda de conversão',
+      'Editar/Excluir',
+    ];
     return (
-      <div />
+      <table>
+        <thead>
+          {headers.map((header, i) => (<th key={ i }>{ header }</th>))}
+        </thead>
+        <tbody>
+          <tr>
+            <td>a</td>
+          </tr>
+        </tbody>
+      </table>
     );
   }
 }
